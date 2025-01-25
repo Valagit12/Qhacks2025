af6ca86e48c3cc531387e46c7cd575d69cc5901e
Valareza Arezehgar <valareza.arezehgar@gmail.com>
2025-01-24 22:29:26 -0500

    pic to text

--- app.py
+++ app.py
@@ -1,23 +1,9 @@
 import os
 import anthropic
 from PIL import Image
+import base64
  
-api_key = os.environ["CLAUDE_KEY"]
-
-
-client = anthropic.Anthropic(
-    # defaults to os.environ.get("ANTHROPIC_API_KEY")
-    api_key=api_key,
-)
-# message = client.messages.create(
-#     model="claude-3-5-sonnet-20241022",
-#     max_tokens=1024,
-#     messages=[
-#         {"role": "user", "content": "Hello, Claude"}
-#     ]
-# )
-
-def resize_image(input_path, output_path, target_size=(200, 200)):
+def resize_image(input_path, output_path, target_size=(500, 500)):
     """
     Resizes an image to the target size and saves it.
 
@@ -35,9 +21,50 @@ def resize_image(input_path, output_path, target_size=(200, 200)):
 
     print(f"Image resized and saved to {output_path}")
 
-# Example usage:
-input_image_path = "./images/alexflex.jpg"
-output_image_path = "./images/resized_image.jpg"
+input_image_path = "./images/alex.jpeg"
+output_image_path = "./images/resized_image.jpeg"
 resize_image(input_image_path, output_image_path)
+
+api_key = os.environ["CLAUDE_KEY"]
+
+
+client = anthropic.Anthropic(
+    # defaults to os.environ.get("ANTHROPIC_API_KEY")
+    api_key=api_key,
+)
+image1_media_type = 'image/jpeg'
+with open(output_image_path, "rb") as img_file:
+        # Read the image as binary
+        img_data = img_file.read()
+
+        # Encode the binary data to base64
+        img_base64 = base64.standard_b64encode(img_data).decode("utf-8")
+        message = client.messages.create(
+            model="claude-3-5-sonnet-20241022",
+            max_tokens=2000,
+            messages=[
+                {
+                    "role": "user",
+                    "content": [
+                        {
+                            "type": "image",
+                            "source": {
+                                "type": "base64",
+                                "media_type": image1_media_type,
+                                "data": img_base64,
+                            },
+                        },
+                        {
+                            "type": "text",
+                            "text": "You are an assistant for the visually impaired. Your job is to describe the scenery showed in the image so they can get a good idea of what their surroundings are."
+                        }
+                    ],
+                }
+            ],
+        )
+print(message)
+
+
+# # Example usage:
 
 
